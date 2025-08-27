5ebc31e4d3104424ddfbc8836dea07cd28f50966
satyakibasu <mail2samratbasu@gmail.com>
2025-08-27 16:31:18 +0530

    Added 1 file into my-rag folder and its contents

--- my-multimodal/image_text_search_v2.py
+++
@@ -1,5 +0,0 @@
-# This program will load all the image embeddings already generated in a json file
-# An image will be used as an input to search based on embeddings. The image will be conbverted to an embeddimgs before search
-# This will be used to do a cosine similarity search using the image embeddings
-
-embeddings_path = '../vector-embeddimgs-demos/embeddings'
